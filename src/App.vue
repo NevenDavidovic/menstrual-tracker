74307f899927280ca9ed2f47b2dfb93bb33f2238
NevenDavidovic <nenoronnie@gmmail.com>
2024-08-10 15:21:38 +0200

    changes to color

--- src/App.vue
+++ src/App.vue
@@ -541,7 +541,6 @@ export default {
 }
 
 .container-layout {
-  background-image: url("/src/assets/img/pexels-codioful-6985003.jpg");
   background-color: #fcd4ca;
 
   width: 100%;
@@ -952,7 +951,7 @@ export default {
 }
 .box:before,
 .box-inner {
-  background-color: #2515d7;
+  background-color: #d77315;
   box-shadow: 2px 2px 12px 0 rgba(0, 0, 0, 0.58);
   background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='1600' height='198'%3e%3cdefs%3e%3clinearGradient id='a' x1='50%25' x2='50%25' y1='-10.959%25' y2='100%25'%3e%3cstop stop-color='%23fcc900' stop-opacity='.25' offset='0%25'/%3e%3cstop stop-color='%23fcc900' offset='100%25'/%3e%3c/linearGradient%3e%3c/defs%3e%3cpath fill='url(%23a)' fill-rule='evenodd' d='M.005 121C311 121 409.898-.25 811 0c400 0 500 121 789 121v77H0s.005-48 .005-77z'/%3e%3c/svg%3e"),
     url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='1600' height='198'%3e%3cdefs%3e%3clinearGradient id='a' x1='50%25' x2='50%25' y1='-10.959%25' y2='100%25'%3e%3cstop stop-color='%23fcc900' stop-opacity='.25' offset='0%25'/%3e%3cstop stop-color='%23fcc900' offset='100%25'/%3e%3c/linearGradient%3e%3c/defs%3e%3cpath fill='url(%23a)' fill-rule='evenodd' d='M.005 121C311 121 409.898-.25 811 0c400 0 500 121 789 121v77H0s.005-48 .005-77z'/%3e%3c/svg%3e"),
